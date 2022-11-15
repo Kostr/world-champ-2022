fbd6fa12971bb92abb15a3088c46add1da93daab
Konstantin Aladyshev <aladyshev22@gmail.com>
2022-11-15 15:31:32 +0300

    Create superuser
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ poetry install
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
 
+if [[ -z $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser
+fi
 python world_champ_2022/manage.py collectstatic --no-input
 python world_champ_2022/manage.py migrate
