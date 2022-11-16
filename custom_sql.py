c1eaeb37061d7915ac9de4346febf0c652a6a4ef
Konstantin Aladyshev <aladyshev22@gmail.com>
2022-11-16 20:33:29 +0300

    Update custom sql
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- custom_sql.py
+++ custom_sql.py
@@ -1,7 +1,6 @@
 from django.db import connection
 
-def my_custom_sql(self):
-    with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM django_migrations WHERE app = socialaccount")
-        cursor.execute("DELETE FROM django_migrations WHERE app = sites")
-    return
+with connection.cursor() as cursor:
+    cursor.execute("DELETE FROM django_migrations WHERE app = socialaccount")
+    cursor.execute("DELETE FROM django_migrations WHERE app = sites")
+
