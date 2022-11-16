be09823bf26aef6b41fbbffc25817fef685f734e
Konstantin Aladyshev <aladyshev22@gmail.com>
2022-11-16 18:03:19 +0300

    Dump fixtures
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- build.sh
+++ build.sh
@@ -16,3 +16,6 @@ fi
 python world_champ_2022/manage.py collectstatic --no-input
 python world_champ_2022/manage.py makemigrations
 python world_champ_2022/manage.py migrate
+
+python world_champ_2022/manage.py dumpdata gambling.Command --indent 4
+python world_champ_2022/manage.py dumpdata gambling.Match --indent 4
