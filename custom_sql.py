846f5effd2c817448714497cbf46fabca3285f95
Konstantin Aladyshev <aladyshev22@gmail.com>
2022-11-17 12:26:12 +0300

    Drop custom_sql.py
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- custom_sql.py
+++
@@ -1,6 +0,0 @@
-from django.db import connection
-
-with connection.cursor() as cursor:
-    cursor.execute("DELETE FROM django_migrations WHERE app = socialaccount")
-    cursor.execute("DELETE FROM django_migrations WHERE app = sites")
-
