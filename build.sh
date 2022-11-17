653a39e5a280dc796af6822f0aca19d1bc8a3d75
Konstantin Aladyshev <aladyshev22@gmail.com>
2022-11-17 15:09:52 +0300

    Add --symlinks to locale compilation
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if [[ -z $CREATE_SUPERUSER ]];
 then
   python world_champ_2022/manage.py createsuperuser --no-input
 fi
-python world_champ_2022/manage.py makemessages -l ru
+python world_champ_2022/manage.py makemessages -l ru --symlinks
 python world_champ_2022/manage.py compilemessages -l ru
 python world_champ_2022/manage.py collectstatic --no-input
 python world_champ_2022/manage.py showmigrations
